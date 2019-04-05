119cda5691f6b3074cf57eade1a425164961cf85
Sebastian Higuita <sebastianhc@kometsales.com>
2019-04-05 08:43:36 -0500

    Additional widgets added

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,6 @@
 export './location.dart';
 export './last_updated.dart';
 export './combined_weather_temperature.dart';
+export './weather_conditions.dart';
+export './temperature.dart';
+export './city_selection.dart';
